9c51b0acb530a2edad95a81a013d477af290827a
gokselk <gokselk.dev@gmail.com>
2025-12-10 20:52:50 +0300

    update patch-vbmeta.sh

--- patch-vbmeta.sh
+++ patch-vbmeta.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-if [ -f vbmeta.img.lz4 ]; then
-	lz4 -B6 --content-size -f vbmeta.img.lz4 vbmeta.img
-fi
+# List of vbmeta partitions to patch
+VBMETA_PARTITIONS="vbmeta vbmeta_system vbmeta_samsung vbmeta_vendor"
 
-./vbmeta-disable-verification vbmeta.img
-rm -rf vbmeta.img.lz4
+for partition in $VBMETA_PARTITIONS; do
+	img_file="${partition}.img"
+	lz4_file="${partition}.img.lz4"
+
+	# Check if lz4 compressed version exists
+	if [ -f "$lz4_file" ]; then
+		echo "Decompressing $lz4_file..."
+		lz4 -B6 --content-size -f "$lz4_file" "$img_file"
+		rm -rf "$lz4_file"
+	fi
+
+	# Patch the vbmeta image if it exists
+	if [ -f "$img_file" ]; then
+		echo "Patching $img_file to disable verification..."
+		./vbmeta-disable-verification "$img_file"
+	fi
+done
+
+echo "vbmeta patching complete!"
